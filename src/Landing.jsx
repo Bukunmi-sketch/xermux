bd5b3ddfc60322ea5fb0807195a647ecd1c334e4
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2022-12-28 22:17:23 +0100

    button.jsx
    button.jsx

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -84,11 +84,11 @@ function LandingPage() {
              
 
                         <a href='http://127.0.0.1:5173/signup'> 
-                        <input type="submit" value="Create New Account" class="signup" onClick={() => {navigate("/signup")}} />
+                        <input type="submit" value="Create New Account" class="signup"  onClick={() => {navigate("/signup")}} />
                         </a>
 
                         <a href='http://127.0.0.1:5173/login'>
-                        <input type="submit" value="Login" class="login" onClick={() => {navigate("/login")}} />
+                        <input type="submit" value="Login" class="login"  />
                         </a>
 
                 </div>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../css/App.css';
+
 
 function Login() {
     return ( 
